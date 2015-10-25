7f14c28dfacc7c56b39a62c05afe01049daf7388
Alan Souza <alansouzati@gmail.com>
2015-10-24 19:26:31 -0700

    Adding app for heroku

--- gulpfile.js
+++ gulpfile.js
@@ -35,10 +35,6 @@ var opts = {
   mainScss: 'src/scss/index.scss',
   webpack: {
     resolve: {
-      alias: { //TODO: remove this, for DEV only
-        'grommet/scss': path.resolve(__dirname, '../grommet/src/scss'),
-        'grommet': path.resolve(__dirname, '../grommet/src/js')
-      },
       root: [
         path.resolve(__dirname, './node_modules')
       ]
