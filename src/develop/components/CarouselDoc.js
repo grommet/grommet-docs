240656d4b9ef10771a6817b4ccad74c251d3046e
Allison Day <day.allison@gmail.com>
2015-11-17 10:22:29 -0800

    Code review fix
    Signed-off-by: Allison Day <day.allison@gmail.com>

--- src/develop/components/CarouselDoc.js
+++ src/develop/components/CarouselDoc.js
@@ -25,13 +25,13 @@ var TileDoc = React.createClass({
           <h2>Options</h2>
           <dl>
             <dt><code>autoplay            true|false</code></dt>
-            <dd>Whether the carousel should play automatically or not.  Default: true.</dd>
+            <dd>Whether the carousel should play automatically or not.  Default is <code>true</code>.</dd>
             <dt><code>autoplaySpeed       {"{number}"}</code></dt>
-            <dd>How long the carousel should stay on each slide, in milliseconds.  Default: 5000 (5 seconds).</dd>
+            <dd>How long the carousel should stay on each slide, in milliseconds.  Default is <code>5000</code> (5 seconds).</dd>
             <dt><code>infinite            true|false</code></dt>
-            <dd>Whether the carousel should scroll back to the first slide when you get to the end, or stop at the last slide.  Default: true.</dd>
+            <dd>Whether the carousel should scroll back to the first slide when you get to the end, or stop at the last slide.  Default is <code>true</code>.</dd>
             <dt><code>persistentNav       true|false</code></dt>
-            <dd>Whether the navigational elements should always be shown, or only show when the user mouses over the carousel.  Default: true.</dd>
+            <dd>Whether the navigational elements should always be shown, or only show when the user mouses over the carousel.  Default is <code>true</code>.</dd>
           </dl>
         </section>
 
