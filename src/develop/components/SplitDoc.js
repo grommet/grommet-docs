de2187055fe3588b9a70b766fa8e82cf8d3e3275
Eric Soderberg <eric.soderberg@hp.com>
2015-11-18 10:16:26 -0800

    Improve description and examples for SidebarDoc and SplitDoc.

--- src/develop/components/SplitDoc.js
+++ src/develop/components/SplitDoc.js
@@ -1,7 +1,12 @@
 var React = require('react');
+var Link = require('react-router').Link;
 var DocsArticle = require('../../DocsArticle');
 var Split = require('grommet/components/Split');
+var Sidebar = require('grommet/components/Sidebar');
+var Article = require('grommet/components/Article');
 var Header = require('grommet/components/Header');
+var Menu = require('grommet/components/Menu');
+var Anchor = require('grommet/components/Anchor');
 var Section = require('grommet/components/Section');
 
 var inline =
@@ -14,7 +19,15 @@ var SplitDoc = React.createClass({
     return (
       <DocsArticle title="Split" colorIndex="neutral-3">
 
-        <p>A full height container with two children laid out horizontally.</p>
+        <p>A full height container with two children laid out horizontally.
+        Typical uses involve placing
+        a <Link to={this.context.routePrefix + "sidebar"}>Sidebar</Link> on
+        the left for primary navigation and then main content on the right.
+        The right side might contain a separate Split component to provide
+        further content depth. Typically, you will want the top Split to be
+        a direct child of
+        the <Link to={this.context.routePrefix + "app"}>App</Link> component
+        as this will provide the best responsive behavior across devices.</p>
         <pre><code className="html hljs xml">{inline}</code></pre>
 
         <section>
@@ -35,18 +48,34 @@ var SplitDoc = React.createClass({
 
           <div className="example">
             <Split>
-              <div>
-                <Header colorIndex="neutral-1" pad={{horizontal: 'medium'}}>Header One</Header>
-                <Section>Content</Section>
-              </div>
-              <div>
-                <Header colorIndex="neutral-2" pad={{horizontal: 'medium'}}>Header Two</Header>
-                <Section>Content</Section>
-              </div>
+              <Sidebar colorIndex="neutral-1">
+                <Header pad={{horizontal: 'medium'}}>Header One</Header>
+                <Menu primary={true}>
+                  <Anchor>Navigation 1</Anchor>
+                  <Anchor>Navigation 2</Anchor>
+                  <Anchor>Navigation 3</Anchor>
+                </Menu>
+              </Sidebar>
+              <Article>
+                <Header pad={{horizontal: 'medium'}}>Header Two</Header>
+                <Section pad="medium">Content</Section>
+              </Article>
             </Split>
           </div>
           <pre><code className="html hljs xml">
-            {"<Split>\n  <p>\n    Sample Content One\n  </p>\n</Split>"}
+            {"<Split>\n" +
+            "  <Sidebar colorIndex=\"neutral-1\">\n" +
+            "    <Header pad={{horizontal: 'medium'}}>Header One</Header>\n" +
+            "    <Menu primary={true}>\n" +
+            "      <Anchor>Navigation 1</Anchor>\n" +
+            "      ...\n" +
+            "    </Menu>\n" +
+            "  </Sidebar>\n" +
+            "  <Article>\n" +
+            "    <Header pad={{horizontal: 'medium'}}>Header Two</Header>\n" +
+            "    <Section pad=\"medium\">Content</Section>\n" +
+            "  </Article>\n" +
+            "</Split>"}
           </code></pre>
 
         </section>
