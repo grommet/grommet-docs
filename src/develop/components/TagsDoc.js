bba826b43100abcd4a8a0b48d0f06edba060780c
Son <phuson@gmail.com>
2016-01-12 13:54:30 -0800

    Added more details to Tags Documentation
    Signed-off-by: Son <phuson@gmail.com>

--- src/develop/components/TagsDoc.js
+++ src/develop/components/TagsDoc.js
@@ -1,12 +1,17 @@
 // (C) Copyright 2014-2015 Hewlett Packard Enterprise Development LP
 
 var React = require('react');
+var Link = require('react-router').Link;
 var DocsArticle = require('../../DocsArticle');
 var Tags = require('grommet/components/Tags');
 var Tag = require('grommet/components/Tag');
 var Box = require('grommet/components/Box');
 
 var TagsDoc = React.createClass({
+
+  contextTypes: {
+    routePrefix: React.PropTypes.string.isRequired
+  },
 
   _onClick: function () {
     // No-op
@@ -15,16 +20,14 @@ var TagsDoc = React.createClass({
   render: function() {
     var inline =
       "<Tags>\n" +
-      "  <Tag>\n" +
-      "    ...\n" +
-      "  </Tag>\n" +
+      "  <Tag />\n" +
       "  ...\n" +
       "</Tags>";
 
     return (
       <DocsArticle title="Tag(s)" colorIndex="neutral-3">
 
-        <p>Lay out equivalently sized Tags of content.</p>
+        <p>A list of tags. The preferred method of populating Tags is to use Tag children.</p>
         <pre><code className="html hljs xml">{inline}</code></pre>
 
         <section>
@@ -32,8 +35,10 @@ var TagsDoc = React.createClass({
           <dl>
             <dt><code>direction    row|column</code></dt>
             <dd>The orientation to layout the child components in.
-              Defaults to <code>column</code>.</dd>
+              Defaults to <code>row</code>.</dd>
           </dl>
+          <p>Options for <Link to={this.context.routePrefix + "box"}>Box</Link> are
+          also available.</p>
         </section>
 
         <section>
@@ -42,6 +47,8 @@ var TagsDoc = React.createClass({
             <dt><code>label         {"{text}"}</code></dt>
             <dd>Label text for the tag.</dd>
           </dl>
+          <p>Options for <Link to={this.context.routePrefix + "anchor"}>Anchor</Link> are
+          also available.</p>
         </section>
 
         <section>
@@ -49,7 +56,7 @@ var TagsDoc = React.createClass({
 
           <p>These examples show a variety of different options for the Tags.</p>
 
-          <h3>default</h3>
+          <h3>Default</h3>
           <Box className="example">
             <Tags>
               <Tag label="First" />
@@ -61,7 +68,7 @@ var TagsDoc = React.createClass({
             {"<Tags>\n  <Tag label='First' />\n  ...\n</Tags>"}
           </code></pre>
 
-          <h3>column</h3>
+          <h3>Column</h3>
           <Box className="example">
             <Tags direction="column">
               <Tag label="First" />
@@ -72,7 +79,6 @@ var TagsDoc = React.createClass({
           <pre><code className="html hljs xml">
             {"<Tags direction=\"column\">\n  <Tag label='First' />\n  ...\n</Tags>"}
           </code></pre>
-
         </section>
 
       </DocsArticle>
