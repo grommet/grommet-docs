f8118b52e9efc3a9c89871539db7ee94862f4731
Alan Souza <alansouzati@gmail.com>
2016-07-18 14:31:33 -0700

    Fixed copy paths

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -22,7 +22,7 @@ gulp.task('set-webpack-alias', () => {
 
 gulp.task('generate-static-site', ['generate-icons-map'], () => {
   var staticSiteConfig = require('./site-generator.webpack.config');
-  options.copyAssets.splice(1);
+  options.copyAssets = options.copyAssets.splice(1);
   return gulp.src(options.mainJs)
       .pipe(gulpWebpack(staticSiteConfig))
       .pipe(gulp.dest(options.dist));
