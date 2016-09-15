fcb105971c3f2547eb645a7155893dd4a7c8f1fc
Alan Souza <alansouzati@gmail.com>
2016-09-15 14:46:19 -0700

    Updated docs for Accordion.

--- src/docs/components/AccordionDoc.js
+++ src/docs/components/AccordionDoc.js
@@ -19,23 +19,23 @@ export default class AccordionDoc extends Component {
         <section>
           <h2>Accordion Options</h2>
           <dl>
+            <dt><code>active        {'{number}|Array({number})'}</code></dt>
+            <dd>Optional active panels to be opened by default.</dd>
             <dt><code>animate             {'{bool}'}</code></dt>
             <dd>Transition content in &amp; out with a slide down animation.
               Defaults to true.</dd>
+            <dt><code>onActive           {'{function}'}</code></dt>
+            <dd>Function that will be called when the Accordion
+              changes the currently active panels.</dd>
             <dt><code>openMulti           {'{bool}'}</code></dt>
             <dd>Allow multiple panels to be opened at once.
               Defaults to false.</dd>
-            <dt><code>initialIndex        {'{number}'}</code></dt>
-            <dd>Optional initial accordion index.</dd>
           </dl>
         </section>
 
         <section>
           <h2>AccordionPanel Options</h2>
           <dl>
-            <dt><code>active              {'{bool}'}</code></dt>
-            <dd>Optional flag to set the panel active (open).
-              Defaults to false.</dd>
             <dt><code>heading             {'{string}|{node}'}</code></dt>
             <dd>Heading of panel, displayed when panel is collapsed.
               Required.</dd>
@@ -105,8 +105,8 @@ export default class AccordionDoc extends Component {
           } />
 
           <Example name="Open multiple, Active" code={
-            <Accordion animate={false} openMulti={true}>
-              <AccordionPanel heading="First Title" active={true}>
+            <Accordion animate={false} openMulti={true} active={[0, 2]}>
+              <AccordionPanel heading="First Title">
                 <h3>First Accordion</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -120,7 +120,67 @@ export default class AccordionDoc extends Component {
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
                   laboris nisi ut aliquip ex ea commodo consequat.</p>
               </AccordionPanel>
-              <AccordionPanel heading="Third Title" active={true}>
+              <AccordionPanel heading="Third Title">
+                <h3>Third Accordion</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.</p>
+              </AccordionPanel>
+            </Accordion>
+          } />
+
+          <Example name="onActive, single" code={
+            <Accordion onActive={
+              (index) => alert(
+                `Active panels: ${index}`
+              )
+            }>
+              <AccordionPanel heading="First Title">
+                <h3>First Accordion</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.</p>
+              </AccordionPanel>
+              <AccordionPanel heading="Second Title">
+                <h3>Second Accordion</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.</p>
+              </AccordionPanel>
+              <AccordionPanel heading="Third Title">
+                <h3>Third Accordion</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.</p>
+              </AccordionPanel>
+            </Accordion>
+          } />
+
+          <Example name="onActive, multiple" code={
+            <Accordion openMulti={true} onActive={
+              (index) => alert(
+                `Active panels: ${index}`
+              )
+            }>
+              <AccordionPanel heading="First Title">
+                <h3>First Accordion</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.</p>
+              </AccordionPanel>
+              <AccordionPanel heading="Second Title">
+                <h3>Second Accordion</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.</p>
+              </AccordionPanel>
+              <AccordionPanel heading="Third Title">
                 <h3>Third Accordion</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
