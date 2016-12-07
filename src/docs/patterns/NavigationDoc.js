4538348d2059c28f05ae46e035a46d1c30a909e9
tracybarmore <tracy.barmore@hpe.com>
2016-12-07 11:25:57 -0800

    Grammar fixes for Navigation page
    - Grammar fix in intro paragraph
    - 3 Grammar fixes in The Header section
    - Grammar fixes in Action Dropdown Menu section

--- src/docs/patterns/NavigationDoc.js
+++ src/docs/patterns/NavigationDoc.js
@@ -17,7 +17,7 @@ export default class NavigationDoc extends Component {
         <Section pad={{between: 'large'}}>
           <Paragraph size='large'>
             Grommet follows a mobile-first design approach for its main
-            navigation. To respond down to smaller device sizes our header
+            navigation. To respond down to smaller device sizes, our header
             consists of only the essential elements of an application. We’ve
             instituded a Hub & Spoke design aesthetic to keep the elements
             minimal and promote a contextual navigation experience.
@@ -44,12 +44,12 @@ export default class NavigationDoc extends Component {
             </Box>
             <Box className='flex'>
               <Paragraph margin='small'>
-                When designing your application the header should only provide
-                the essential elements of navigation. The header composition is
-                formed by a combination of the <em>Application Icon</em>,
+                When designing your application, the header should only provide
+                the essential elements of the navigation. The header composition is
+                formed by a combination of the <em>Application Icon</em>, 
                 <em>Application Name</em>, and when available, the <em>User
                   Functional Icon</em>,
-                  and/or <em>Search Field</em>/<em>Icon</em>.
+                  and/or <em>Search Field Icon</em>.
               </Paragraph>
               <Paragraph margin='small'>
                 If your application requires the exposure of other navigational
@@ -65,7 +65,7 @@ export default class NavigationDoc extends Component {
 
         <Section separator='bottom'>
           <Paragraph size='large'>
-            <strong>The Sidebar</strong> When designing your {'application\'s'}
+            <strong>The Sidebar</strong>. When designing your {'application\'s'} 
               main navigation, your sidebar is your friend. All key application
               areas should be accessible through your sidebar.
           </Paragraph>
@@ -93,7 +93,7 @@ export default class NavigationDoc extends Component {
 
         <Section separator='bottom'>
           <Paragraph size='large'>
-            <strong>Action Dropdown Menu</strong> In some cases the application
+            <strong>Action Dropdown Menu</strong>. In some cases, the application
               menu items will necessitate a need for secondary actions. When
               these cases are presented, the dropdown component can be used in
               conjunction with a functional icon.
@@ -109,7 +109,7 @@ export default class NavigationDoc extends Component {
                 relevant to the function of the icon. The dropdown menu
                 component only supports a <strong>single level of
                 structure</strong>. If you are trying to expose more granular
-                actions within a menu or sub-Sections, these actions should be
+                actions within a menu or sub-sections, these actions should be
                 moved outside of the dropdown menu or into a separate display
                 mechanism in your application.
               </Paragraph>
@@ -126,7 +126,7 @@ export default class NavigationDoc extends Component {
 
         <Section separator='bottom'>
           <Paragraph size='large'>
-            <strong>Responsive Behavior</strong> Much like all components in
+            <strong>Responsive Behavior</strong>. Much like all components in
               Grommet, the navigation automagically responds to display as much
               context as possible for users at different browser sizes and
               devices.
