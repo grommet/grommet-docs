fbc564447345d215d579543d0c4f885bb3c56fcf
Alan Souza <alansouzati@gmail.com>
2016-12-07 10:32:21 -0800

    Updated Accessibility page with latest content.

--- src/docs/reference/Accessibility.js
+++ src/docs/reference/Accessibility.js
@@ -2,7 +2,13 @@
 
 import React, { Component } from 'react';
 import DocsArticle from '../../components/DocsArticle';
-import Table from 'grommet/components/Table';
+import Code from '../../components/Code';
+import App from 'grommet/components/App';
+import Anchor from 'grommet/components/Anchor';
+import Heading from 'grommet/components/Heading';
+import SkipLinkAnchor from 'grommet/components/SkipLinkAnchor';
+import Footer from 'grommet/components/Footer';
+import Box from 'grommet/components/Box';
 import Status from 'grommet/components/icons/Status';
 
 export default class Accessibility extends Component {
@@ -17,58 +23,59 @@ export default class Accessibility extends Component {
 
         <section>
           <p>Products that are accessible to all users are good for people,
-          products, and business.  All users should be empowered with access
-          and the ability to have a pleasant experience with your
-          application.</p>
+          products, and business. All users should be empowered with access
+          and the ability to have a pleasant experience.</p>
 
-          <p>We follow the <a href='http://www.w3.org/TR/WCAG20/'>Web Content
-          Accessibility Guidelines (WCAG)</a>.  By following this style guide
-          and utilizing the accompanying implementation platform, you will be
-          well on your way to satisfying the WCAG recommendations.</p>
+          <p>Grommet follows the <Anchor href='http://www.w3.org/TR/WCAG20/'>
+            Web Content Accessibility Guidelines (WCAG)
+          </Anchor>.  By following this style guide
+          and utilizing Grommet, you will be
+          well on your way to satisfying the WCAG recommendations.
+          </p>
+
+          <p>
+            Grommet will add the basic accessibility support automatically.
+            For example, all our graphics are accessible and keyboard operable.
+            The following sections will describe additional features that will
+            allow your application to be even more accessible.
+          </p>
         </section>
 
         <section>
-          <p>This section describes the Grommet guidelines for developing
-            accessible applications.</p>
+          <Heading tag='h2'>Custom title</Heading>
+          <p>Grommet components can be read
+            by <Anchor
+              href="https://www.afb.org/ProdBrowseCatResults.asp?CatID=49">
+              screen readers
+            </Anchor>. The default
+            textual description varies for each component.
+            In general, this description is generic and lacks contextual
+            information that help users to understand the surrounding data.
+            Most Grommet components support a property called <b>
+              a11yTitle
+            </b> that enables callers to provide a
+            better description.
+            </p>
+          <Box>
+            <Heading tag='h4'>Example:</Heading>
+            <p>Using <b>a11yTitle</b> for icons to explain their purpose.</p>
 
-          <h3>Icons</h3>
-          <p>The icon components can be read by screen readers. The default
-            textual description for icons can be overridden by setting
-            the a11yTitle. The default title or a11yTitle attribute use
-            localization if it exist.</p>
-          <h4>Example:</h4>
-          <Table>
-            <caption>Example of Icons with different values</caption>
-            <thead>
-              <tr>
-              <th>Icon</th>
-              <th>Description</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td><Status value='critical'/></td>
-                <td><pre><code className='html hljs xml'>
-                  {"<Status value='critical'>"}</code></pre></td>
-              </tr>
-              <tr>
-                <td><Status value='critical' a11yTitle='Server Down'/></td>
-                <td><pre><code className='html hljs xml'>
-                  {"<Status value='critical' a11yTitle='Server Down'>"}
-                </code></pre></td>
-              </tr>
-            </tbody>
-          </Table>
+            <Box direction='row' align='center' pad={{ between: 'small' }}>
+              <Status a11yTitle='Server Down' value='critical'/>
+              <Code>
+                <Status a11yTitle='Server Down' value='critical'/>
+              </Code>
+            </Box>
+          </Box>
 
-          <h3>Menu</h3>
           <p>
-            The Grommet Menu component also supports screen readers. By default,
-            the Menu component assumes the "menu" role and the focusable
-            children passed to this component receive the "menuitem" role. The
-            user can navigate the Menu by using either the tab or arrow keys.
+            For this example, will read "Server Down, Image" instead of
+            "Critical, Image".
           </p>
+        </section>
 
-          <h3>Lang attribute</h3>
+        <section>
+          <Heading tag='h2'>Language support</Heading>
           <p>
             If the lang attribute is not explicitly set in the html
             element, Grommet will specify the lang attribute according to
@@ -76,30 +83,39 @@ export default class Accessibility extends Component {
             lang attribute can be set on other elements like App.
           </p>
           <h4>Example</h4>
-          <pre><code className='html hljs xml'>
-            {"<App lang='en-US'>\n  ...\n</App>"}
-          </code></pre>
-          <h3>Skip Links</h3>
+          <Code>
+            <App lang='en-US'>Testing</App>
+          </Code>
+        </section>
+
+        <section>
+          <Heading tag='h2'>Skip Links</Heading>
           <p>Grommet has skip links that make it easy to skip repetitive
-            content. Grommet skip links have two locations: Skip to Main
-            Content and Skip to Footer. To set the "Skip to Main Content"
-            link in Grommet, an attribute primary="true" needs to be added
-            to the main content element. The "Skip to Footer" link is
-            added by default with the Footer component. </p>
+            content.
+            By default, Grommet skip links have two locations: Skip to Main
+            Content and Skip to Footer.
+            To set the "Skip to Main Content"
+            link in Grommet, an attribute <b>primary="true"</b> needs to be
+            added to a <Anchor path='/docs/box'>Box</Anchor> element.
+            The "Skip to Footer" link works in a similar way by
+            adding <b>primary="true"</b> to a <Anchor path='/docs/footer'>
+              Footer
+            </Anchor> component. Custom skip links are possible by creating
+            an instance of a <b>SkipLinkAnchor</b>.
+            </p>
+          <p>Skip links will be automatically presented when pressing tab in a
+            given page.</p>
           <h4>Example:</h4>
-          <pre><code className='html hljs xml'>
-            {"<App>\n" +
-            "  <Article>\n" +
-            "    <Header>\n" +
-            "      <h1>Title</h1>\n" +
-            "    </Header>\n" +
-            "    <Section primary={true}>\n" +
-            "      <h2>Heading</h2>\n" +
-            "      <p>Lorem ipsum ...</p>\n" +
-            "    </Section>\n" +
-            "  </Article>\n" +
-            "</App>"}
-          </code></pre>
+          <Code>
+            <App>
+              <Box primary={true}>Main Content</Box>
+              <Box>
+                <SkipLinkAnchor label='Custom' />
+                Custom skip link
+              </Box>
+              <Footer primary={true}>Application Footer</Footer>
+            </App>
+          </Code>
         </section>
       </DocsArticle>
     );
