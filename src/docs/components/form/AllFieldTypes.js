485d67c4078e59edfa4acfa5729720f0cb0949f9
Eric Soderberg <eric.soderberg@hpe.com>
2017-01-12 11:10:52 -0800

    Refactored state management of AllFieldTypes form sample.

--- src/docs/components/form/AllFieldTypes.js
+++ src/docs/components/form/AllFieldTypes.js
@@ -18,102 +18,83 @@ export default class AllFieldTypes extends Component {
 
   constructor () {
     super();
-    this._onChangeRange = this._onChangeRange.bind(this);
-    this._onChange = this._onChange.bind(this);
-    this._onSearchInputChange = this._onSearchInputChange.bind(this);
-    this._onDateTimeChange = this._onDateTimeChange.bind(this);
-    this._onTextInputDOMChange = this._onTextInputDOMChange.bind(this);
-    this._onTextInputSelect = this._onTextInputSelect.bind(this);
-    this._onSelectSearch = this._onSelectSearch.bind(this);
-    this._onSelectChange = this._onSelectChange.bind(this);
     this.state = {
-      rangeValue: 10,
-      searchInput: { suggestions: VALUES },
-      select: { options: VALUES },
-      textInput: { suggestions: VALUES, value: '' },
-      calendarDate: (new Date()).toISOString().slice(0, 10)
+      boolean: false,
+      date: (new Date()).toISOString().slice(0, 10),
+      number: 10,
+      option: '',
+      suggestions: VALUES,
+      text: ''
     };
   }
 
-  _onChangeRange (event) {
-    this.setState({rangeValue: parseInt(event.target.value, 10)});
-  }
-
-  _onChange (event) {
-    // console.log('!!! FullForm changed', event.target, 'to',
-    //   event.target.value);
-  }
-
-  _onSearchInputChange (value) {
-    // console.log('!!! FullForm _onSearchInputChange', value);
-    this.setState({
-      searchInput: {
-        value: value,
-        suggestions: this._searchInputSuggestions
+  _change (type) {
+    return (event) => {
+      const nextState = { suggestions: VALUES };
+      let value;
+      if ('boolean' === type) {
+        value = event.target.checked;
+      } else if ('number' === type) {
+        value = parseInt(event.target.value, 10);
+      } else if (event.hasOwnProperty('suggestion')) {
+        value = event.suggestion;
+      } else if (event.hasOwnProperty('value')) {
+        value = event.value;
+      } else if (event.hasOwnProperty('target')) {
+        value = event.target.value;
+      } else {
+        value = event;
       }
-    });
+      nextState[type] = value;
+      this.setState(nextState);
+    };
   }
 
-  _onTextInputDOMChange (event) {
-    const regexp = new RegExp('^' + event.target.value);
-    const suggestions = VALUES.filter(val => {
-      return regexp.test(val);
-    });
-    this.setState({
-      textInput: { value: event.target.value, suggestions: suggestions }
-    });
-  }
-
-  _onTextInputSelect (pseudoEvent) {
-    this.setState({
-      textInput: { value: pseudoEvent.suggestion, suggestions: VALUES }
-    });
-  }
-
-  _onSelectSearch (event) {
-    const regexp = new RegExp('^' + event.target.value);
-    const options = VALUES.filter(val => regexp.test(val));
-    this.setState({ select: { options: options } });
-  }
-
-  _onSelectChange (pseudoEvent) {
-    this.setState({ select: { value: pseudoEvent.option, options: VALUES } });
-  }
-
-  _onDateTimeChange (value) {
-    this.setState({ date: value });
+  _domChange (type) {
+    return (event) => {
+      const value = event.target.value;
+      const regexp = new RegExp('^' + value);
+      const suggestions = VALUES.filter(val => regexp.test(val));
+      const nextState = { suggestions };
+      if ('text' === type) {
+        nextState[type] = value;
+      }
+      this.setState(nextState);
+    };
   }
 
   render () {
+    const { boolean, date, number, option, suggestions, text } = this.state;
     return (
       <fieldset>
 
         <legend>Text</legend>
-        <FormField label='input' htmlFor='f-input'
+        <FormField label='TextInput' htmlFor='text-input'
           help={<span>sample help <Anchor>learn more ...</Anchor></span>}>
+          <TextInput id='text-input'
+            value={text} suggestions={suggestions}
+            onDOMChange={this._domChange('text')}
+            onSelect={this._change('text')} />
+        </FormField>
+        <FormField label='input[text]' htmlFor='f-input'>
           <input id='f-input' name='input' type='text'
-            onChange={this._onChange} />
+            value={text} onChange={this._change('text')} />
         </FormField>
         <FormField label='textarea' htmlFor='f-textarea'
           error='something is wrong'>
-          <textarea id='f-textarea' name='textarea'/>
-        </FormField>
-        <FormField label='TextInput' htmlFor='text-input'>
-          <TextInput id='text-input'
-            value={this.state.textInput.value}
-            suggestions={this.state.textInput.suggestions}
-            onDOMChange={this._onTextInputDOMChange}
-            onSelect={this._onTextInputSelect} />
+          <textarea id='f-textarea' name='textarea'
+            value={text} onChange={this._change('text')} />
         </FormField>
 
         <legend>Toggles</legend>
         <FormField>
           <CheckBox id='f-check-box' name='check' label='CheckBox'
-            onChange={this._onChange} />
+            checked={boolean} onChange={this._change('boolean')} />
         </FormField>
         <FormField>
           <CheckBox id='f-toggle' name='toggle' label='CheckBox toggle'
-            toggle={true} onChange={this._onChange} />
+            toggle={true}
+            checked={boolean} onChange={this._change('boolean')} />
         </FormField>
         <FormField label='RadioButton'>
           <RadioButton id='f-radio-1' name='radio' label='RadioButton 1'
@@ -123,39 +104,27 @@ export default class AllFieldTypes extends Component {
         </FormField>
 
         <legend>Other</legend>
-        {/*}
-        <FormField label='select' htmlFor='f-select'>
-          <select id='f-select' name='select'>
-            <option>first</option>
-            <option>second</option>
-            <option>third</option>
-          </select>
-        </FormField>
-        {*/}
         <FormField label='Select' htmlFor='f-Select'>
           <Select id='f-Select' name='Select'
-            value={this.state.select.value}
-            options={this.state.select.options}
-            onSearch={this._onSelectSearch}
-            onChange={this._onSelectChange} />
-        </FormField>
-        <FormField label='NumberInput' htmlFor='f-number-input'>
-          <NumberInput id='f-number-input' name='number'
-            value={this.state.rangeValue}
-            onChange={this._onChangeRange} />
+            value={option} options={suggestions}
+            onSearch={this._domChange('option')}
+            onChange={this._change('option')} />
         </FormField>
         <FormField label='DateTime' htmlFor='f-date-time'>
           <DateTime id='f-date-time' name='date-time'
             format='YYYY-MM-DD h:mm a' step={5}
-            value={this.state.date}
-            onChange={this._onDateTimeChange} />
+            value={date} onChange={this._change('date')} />
+        </FormField>
+        <FormField label='NumberInput' htmlFor='f-number-input'>
+          <NumberInput id='f-number-input' name='number'
+            value={number} onChange={this._change('number')} />
         </FormField>
         <FormField label='input - range' htmlFor='f-range'
-          help={this.state.rangeValue}>
+          help={number}>
           <input id='f-range' name='range' type='range'
-            min='1' max='20' value={this.state.rangeValue}
-            onChange={this._onChangeRange}
-            onMouseUp={this._onChangeRange}/>
+            min='1' max='20' value={number}
+            onChange={this._change('number')}
+            onMouseUp={this._change('number')}/>
         </FormField>
         <FormField label='input - file' htmlFor='f-file'>
           <input id='f-file' name='file' type='file' />
--- src/docs/components/samples/FullForm.js
+++ src/docs/components/samples/FullForm.js
@@ -3,166 +3,26 @@
 import React, { Component, PropTypes } from 'react';
 import Form from 'grommet/components/Form';
 import FormFields from 'grommet/components/FormFields';
-import FormField from 'grommet/components/FormField';
 import Header from 'grommet/components/Header';
-import CheckBox from 'grommet/components/CheckBox';
-import RadioButton from 'grommet/components/RadioButton';
-import SearchInput from 'grommet/components/SearchInput';
-import NumberInput from 'grommet/components/NumberInput';
-import Table from 'grommet/components/Table';
+import Heading from 'grommet/components/Heading';
 import Footer from 'grommet/components/Footer';
 import Button from 'grommet/components/Button';
-import DateTime from 'grommet/components/DateTime';
-
-const SUGGESTIONS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven',
-  'eight'];
+import AllFieldTypes from '../form/AllFieldTypes';
 
 export default class FullForm extends Component {
 
-  constructor () {
-    super();
-    this._onChangeRange = this._onChangeRange.bind(this);
-    this._onChange = this._onChange.bind(this);
-    this._onSearchInputChange = this._onSearchInputChange.bind(this);
-    this._onDateTimeChange = this._onDateTimeChange.bind(this);
-    this._onSearch = this._onSearch.bind(this);
-    this.state = {
-      rangeValue: 10,
-      searchInput: {suggestions: SUGGESTIONS},
-      calendarDate: (new Date()).toISOString().slice(0, 10)
-    };
-  }
-
-  _onChangeRange (event) {
-    this.setState({rangeValue: event.target.value});
-  }
-
-  _onChange (event) {
-    // console.log('!!! FullForm changed', event.target, 'to',
-    //   event.target.value);
-  }
-
-  _onSearchInputChange (value) {
-    // console.log('!!! FullForm _onSearchInputChange', value);
-    this.setState({
-      searchInput: {
-        value: value,
-        suggestions: this._searchInputSuggestions
-      }
-    });
-  }
-
-  _onDateTimeChange (value) {
-    this.setState({ date: value });
-  }
-
-  _onSearch (text) {
-    const searchInput = this.state.searchInput;
-    const regexp = new RegExp('^' + text);
-    searchInput.suggestions =
-      this._searchInputSuggestions.filter(function (value) {
-        return regexp.test(value);
-      });
-    this.setState({searchInput: searchInput});
-  }
-
   render () {
-    const p = this.props.prefix;
-
+    const { compact, onSubmit } = this.props;
     return (
-      <Form onSubmit={this.props.onSubmit} compact={this.props.compact}>
+      <Form onSubmit={onSubmit} compact={compact}>
         <Header>
-          <h1>Edit</h1>
+          <Heading tag='h1' margin='none'>Edit</Heading>
         </Header>
         <FormFields>
-          <fieldset>
-            <legend>First section</legend>
-            <FormField label="Item 1" htmlFor={p + "item1"}
-              help="something helpful">
-              <input id={p + "item1"} name="item-1" type="text"
-                onChange={this._onChange} />
-            </FormField>
-            <FormField>
-              <CheckBox id={p + "item2"} name="item-2" label="Item 2"
-                onChange={this._onChange} />
-            </FormField>
-            <FormField>
-              <CheckBox id={p + "item3"} name="item-3" label="Item 3"
-                toggle={true}
-                onChange={this._onChange} />
-            </FormField>
-            <FormField label="Item 4">
-              <RadioButton id={p + "item4-1"} name="item-4" label="first"
-                onChange={this._onChange} />
-              <RadioButton id={p + "item4-2"} name="item-4" label="second"
-                onChange={this._onChange} />
-            </FormField>
-            <FormField label="Item 5" htmlFor={p + "item5"}
-              error="something's wrong">
-              <textarea id={p + "item5"} name="item-5" />
-            </FormField>
-            <FormField label="Item 6" htmlFor={p + "item6"}>
-              <SearchInput id={p + "item6"} name="item-6"
-                value={this.state.searchInput.value}
-                suggestions={this.state.searchInput.suggestions}
-                onChange={this._onSearchInputChange}
-                onSearch={this._onSearchInputSearch} />
-            </FormField>
-            <FormField label="Item 7" htmlFor={p + "item7"}
-              help={<a>learn more ...</a>}>
-              <select id={p + "item7"} name="item-7">
-                <option>first</option>
-                <option>second</option>
-                <option>third</option>
-              </select>
-            </FormField>
-            <FormField label="Item 9" htmlFor={p + "item9"}>
-              <NumberInput id={p + "item9"} name="item-9"
-                value={this.state.rangeValue}
-                onChange={this._onChangeRange} />
-            </FormField>
-            <FormField label="Item 10" htmlFor={p + "item10"}
-              help={this.state.rangeValue}>
-              <input id={p + "item10"} name="item-10" type="range"
-                min="1" max="20" value={this.state.rangeValue}
-                onChange={this._onChangeRange}
-                onMouseUp={this._onChangeRange}/>
-            </FormField>
-          </fieldset>
-          <fieldset>
-            <legend>Another section</legend>
-            <p>Some informational text.</p>
-            <FormField label="Item 11">
-              <Table selectable={true}>
-                <tbody>
-                  <tr>
-                    <td>first</td>
-                    <td>123</td>
-                  </tr>
-                  <tr>
-                    <td>second</td>
-                    <td>456</td>
-                  </tr>
-                  <tr>
-                    <td>third</td>
-                    <td>789</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </FormField>
-            <FormField label="Item 12" htmlFor={p + "item12"}>
-              <input id={p + "item12"} name="item-12" type="file" />
-            </FormField>
-            <FormField label="Item 13" htmlFor={p + "item13"}>
-              <DateTime id={p + "item13"} name="item-13"
-                format="YYYY-MM-DD h:mm a" step={5}
-                value={this.state.date}
-                onChange={this._onDateTimeChange} />
-            </FormField>
-          </fieldset>
+          <AllFieldTypes />
         </FormFields>
         <Footer pad={{vertical: 'medium'}}>
-          <Button label="OK" primary={true} onClick={this.props.onSubmit} />
+          <Button label="OK" primary={true} onClick={onSubmit} />
         </Footer>
       </Form>
     );
@@ -172,10 +32,5 @@ export default class FullForm extends Component {
 FullForm.propTypes = {
   compact: PropTypes.bool,
   onCancel: PropTypes.func,
-  onSubmit: PropTypes.func,
-  prefix: PropTypes.string
-};
-
-FullForm.defaultProps = {
-  prefix: 'ff'
+  onSubmit: PropTypes.func
 };
