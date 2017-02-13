ea2e681832b6bc54852b02e5533d09e94938ea94
Douglas Gennetten <douglas@gennetten.com>
2017-02-13 12:28:32 -0700

    #228 Update Hello World page (#242)
    * #228 Update Hello World page
    
    * removed unused Anchor import
    
    * Added missing Paragraph import

--- src/docs/guides/HelloWorld.js
+++ src/docs/guides/HelloWorld.js
@@ -1,7 +1,8 @@
 // (C) Copyright 2014-2016 Hewlett Packard Enterprise Development LP
 
 import React, { Component } from 'react';
-import Anchor from 'grommet/components/Anchor';
+import Button from 'grommet/components/Button';
+import Paragraph from 'grommet/components/Paragraph';
 import DocsArticle from '../../components/DocsArticle';
 
 export default class HelloWorld extends Component {
@@ -10,23 +11,22 @@ export default class HelloWorld extends Component {
     return (
       <DocsArticle title="Hello World">
         <section>
-          <p>
+          <Paragraph size='large'>
             The easiest way to learn a framework is by writing a simple
-            application.
-            In this section you will access the Grommet source from our
-            server and
-            you will be able to experiment with Grommet using just an HTML
-            file and
-            your browser.  {"You'll"} use hosted JavaScript files in this
-            exercise which
-            is not meant to be used in production.
-          </p>
-          <p>
+            application. In this section you will access the Grommet 
+            source code from our server and you will be able to 
+            experiment with Grommet using just an HTML
+            file and your browser.  
+          </Paragraph>
+          <Paragraph size='large'> 
             To help you get started quickly, we provide the
             following <a
               href="https://codepen.io/pen?template=gaEGPY&editors=0010"
               target="_blank">CodePen</a> template:
-          </p>
+          </Paragraph>
+          <Paragraph size='small'>
+            For this example we will be using hosted JavaScript files
+            which are not meant to be used in production.</Paragraph>
           <iframe height='268' scrolling='no'
             src={"//codepen.io/grommet/embed/gaEGPY/?" +
               "height=268&theme-id=0&default-tab=result"}
@@ -34,26 +34,26 @@ export default class HelloWorld extends Component {
             style={{width: '100%'}}>See the Pen
             <a href='https://codepen.io/grommet/pen/gaEGPY/'>
               Grommet Hello World</a>
-            by Grommet UX (<a href='https://codepen.io/grommet'>@grommet</a>) on
-            <a href='https://codepen.io'>CodePen</a>.
+            by Grommet UX (<a href='https://codepen.io/grommet'>@grommet</a>) 
+            on <a href='https://codepen.io'>CodePen</a>.
           </iframe>
-          <p>
-            Also, there is a
-            Github <a
-            href="https://gist.github.com/grommetux/fb257dcd546a58070db9"
-            target="_blank">Gist</a> that you can use to load this same
-            Hello World app in your local computer.
-          </p>
+          <Paragraph size='small'>
+            We also provided a 
+            <a href="https://gist.github.com/grommetux/fb257dcd546a58070db9"
+            target="_blank"> Github Gist</a> to load this Hello World
+            application on your local system.
+          </Paragraph>
         </section>
         <section>
           <h2>Next Steps</h2>
-          <p>
+          <Paragraph>
             Now that {"you've"} already played with Grommet, we recommend that
-            you check
-            out the <Anchor path="/docs/get-started">
-            Get Started</Anchor> page. There you will learn
+            you check out the Get Started section. There you will learn
             how to install Grommet in your local environment.
-          </p>
+          </Paragraph>
+          <Button label='Get Started'
+            primary={true}
+            href='https://grommet.github.io/docs/get-started'/>
         </section>
       </DocsArticle>
     );
