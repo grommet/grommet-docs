b3f0841579a65bfc3e5f5b2476b7519f80d08eae
Eric Soderberg <eric.soderberg@hpe.com>
2017-02-21 16:14:51 -0800

    Fixed Hello World link.

--- src/docs/guides/HelloWorld.js
+++ src/docs/guides/HelloWorld.js
@@ -13,12 +13,12 @@ export default class HelloWorld extends Component {
         <section>
           <Paragraph size='large'>
             The easiest way to learn a framework is by writing a simple
-            application. In this section you will access the Grommet 
-            source code from our server and you will be able to 
+            application. In this section you will access the Grommet
+            source code from our server and you will be able to
             experiment with Grommet using just an HTML
-            file and your browser.  
+            file and your browser.
           </Paragraph>
-          <Paragraph size='large'> 
+          <Paragraph size='large'>
             To help you get started quickly, we provide the
             following <a
               href="https://codepen.io/pen?template=gaEGPY&editors=0010"
@@ -34,11 +34,11 @@ export default class HelloWorld extends Component {
             style={{width: '100%'}}>See the Pen
             <a href='https://codepen.io/grommet/pen/gaEGPY/'>
               Grommet Hello World</a>
-            by Grommet UX (<a href='https://codepen.io/grommet'>@grommet</a>) 
+            by Grommet UX (<a href='https://codepen.io/grommet'>@grommet</a>)
             on <a href='https://codepen.io'>CodePen</a>.
           </iframe>
           <Paragraph size='small'>
-            We also provided a 
+            We also provided a
             <a href="https://gist.github.com/grommetux/fb257dcd546a58070db9"
             target="_blank"> Github Gist</a> to load this Hello World
             application on your local system.
@@ -51,9 +51,7 @@ export default class HelloWorld extends Component {
             you check out the Get Started section. There you will learn
             how to install Grommet in your local environment.
           </Paragraph>
-          <Button label='Get Started'
-            primary={true}
-            href='https://grommet.github.io/docs/get-started'/>
+          <Button label='Get Started' primary={true} path='/docs/get-started'/>
         </section>
       </DocsArticle>
     );
