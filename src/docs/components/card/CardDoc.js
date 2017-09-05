89ba9b738c1a822e82d160dc79525536e94e3fde
karatechops <contact@lazers.tv>
2017-09-05 16:35:12 -0400

    Lint Card's description deprecation notice.

--- src/docs/components/card/CardDoc.js
+++ src/docs/components/card/CardDoc.js
@@ -51,10 +51,11 @@ export default class CardDoc extends Component {
               </code></dt>
             <dd>Either a string in markdown syntax or an element.
               See <Anchor path='/docs/markdown'>
-              Markdown</Anchor> for additional details. <strong>Deprecation Notice: </strong>
-              Implicit Markdown support will be removed in the next major release.
-              Description will continue to accept elements as a description
-              property.</dd>
+              Markdown</Anchor> for additional details.
+              <strong>Deprecation Notice: </strong>
+              Implicit Markdown support will be removed in the next major 
+              release.Description will continue to accept elements as a
+              description property.</dd>
             <dt><code>heading              {'{string}|{element}'}</code></dt>
             <dd>Heading content.</dd>
             <dt><code>headingStrong        true|false</code></dt>
