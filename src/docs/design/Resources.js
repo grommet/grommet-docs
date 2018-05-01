4964571b44a3bf840a471f86b8c6cbaa28b755ce
Chris Carlozzi <lozzi@Lozzi.local>
2018-05-01 10:10:49 -0700

    Link Cleaup
    Removed old links and streamlined layout.

--- src/docs/design/Resources.js
+++ src/docs/design/Resources.js
@@ -18,151 +18,54 @@ export default class Resources extends Component {
         </p>
 
         <section id="grommet">
-          <h2>Grommet</h2>
-
-          <h4>Sketch</h4>
-
-          <ul>
-            <li>
-              <a id="grommet-components-sketch"
-                href={`${ROOT}/grommet/grommet-components-1.1.sketch`}
-                target="_blank">Components Sticker Sheet v1.1</a>
-            </li>
-          </ul>
-
+        <h2>Grommet</h2>
+          <a id="grommet-components-sketch"
+            href={`${ROOT}/grommet/grommet-components-1.1.sketch`}
+            target="_blank">Components Sticker Sheet v1.1 (Sketch)</a>
         </section>
 
         <section id="hpe">
-          <h2>Hewlett Packard Enterprise</h2>
-
-          <h4>Sketch</h4>
-
-          <ul>
-            <li>
-              <a id="hpe-components-sketch"
-                href={`${ROOT}/hpe/hpe-components-1.1.sketch`}
-                target="_blank">Components Sticker Sheet v1.1</a>
-            </li>
-            <li>
-              <a id="hpe-visualizations-sketch"
-                href={`${ROOT}/hpe/hpe-visualizations-1.0.sketch`}
-                target="_blank">Visualizations Sticker Sheet v1.0</a>
-            </li>
-            <li>
-              <a id="hpe-icons-sketch"
-                href={`${ROOT}/hpe/hpe-icons-1.2.sketch`}
-                target="_blank">Icons Sticker Sheet v1.2</a>
-            </li>
-            <li>
-              <a id="hpe-templates-sketch"
-                href={`${ROOT}/hpe/hpe-templates-0.9.sketch`}
-                target="_blank">Templates Sticker Sheet v0.9</a>
-            </li>
-          </ul>
-
-          <h4>PDF</h4>
-
-          <ul>
-            <li>
-              <a id="hpe-components-pdf"
-                href={`${ROOT}/hpe/hpe-components-1.0.pdf`}
-                target="_blank">Components Sticker Sheet v1.0</a>
-            </li>
-            <li>
-              <a id="hpe-visualizations-pdf"
-                href={`${ROOT}/hpe/hpe-visualizations-1.0.pdf`}
-                target="_blank">Visualizations Sticker Sheet v1.0</a>
-            </li>
-            <li>
-              <a id="hpe-icons-pdf"
-                href={`${ROOT}/hpe/hpe-icons-1.1.pdf`}
-                target="_blank">Icons Sticker Sheet v1.1</a>
-            </li>
-            <li>
-              <a id="hpe-templates-pdf"
-                href={`${ROOT}/hpe/hpe-templates-0.9.pdf`}
-                target="_blank">Templates Sticker Sheet v0.9</a>
-            </li>
-          </ul>
-
-          <h4>Other Document Types</h4>
-          <p>
-            To find Adobe Illustrator, Balsamiq, and Axure files please visit
-            the <a href={`${ROOT}/hpe`} target="_blank">Archive</a>.
-            These files do not represent the latest design
-            direction of Hewlett Packard Enterprise.
-          </p>
-
+        <h2>Hewlett Packard Enterprise</h2>
+          <a id="hpe-components-sketch"
+            href={`${ROOT}/hpe/hpe-components-1.1.sketch`}
+            target="_blank">Components Sticker Sheet v1.1 (Sketch)</a>
+          <a id="hpe-visualizations-sketch"
+            href={`${ROOT}/hpe/hpe-visualizations-1.0.sketch`}
+            target="_blank">Visualizations Sticker Sheet v1.0 (Sketch)</a>
+          <a id="hpe-icons-sketch"
+            href={`${ROOT}/hpe/hpe-icons-1.6.sketch`}
+            target="_blank">Icons Sticker Sheet v1.6 (Sketch)</a>
+          <a id="hpe-templates-sketch"
+            href={`${ROOT}/hpe/hpe-templates-1.0.sketch`}
+            target="_blank">Templates Sticker Sheet v1.0 (Sketch)</a>
         </section>
 
         <section id="aruba">
-          <h2>Aruba</h2>
-
-          <h4>Sketch</h4>
-
-          <ul>
-            <li>
-              <a id="aruba-components-sketch"
-                href={`${ROOT}/aruba/aruba-components-1.0.sketch`}
-                target="_blank">Components Sticker Sheet v1.0</a>
-            </li>
-          </ul>
-
+        <h2>Aruba</h2>
+          <a id="aruba-components-sketch"
+            href={`${ROOT}/aruba/aruba-components-1.0.sketch`}
+            target="_blank">Components Sticker Sheet v1.0 (Sketch)</a>
         </section>
 
         <section id="micro-focus">
-          <h2>Micro Focus</h2>
-
-          <h4>Sketch</h4>
-
-          <ul>
-            <li>
-              <a id="micro-focus-components-sketch"
-                href={`${ROOT}/micro-focus/micro-focus-components-1.0.sketch`}
-                target="_blank">Components Sticker Sheet v1.0</a>
-            </li>
-          </ul>
-
+        <h2>Micro Focus</h2>
+          <a id="micro-focus-components-sketch"
+            href={`${ROOT}/micro-focus/micro-focus-components-1.0.sketch`}
+            target="_blank">Components Sticker Sheet v1.0 (Sketch)</a>
         </section>
 
         <section id="dxc">
-          <h2>DXC</h2>
-
-          <h4>Sketch</h4>
-
-          <ul>
-            <li>
-              <a id="dxc-components-sketch"
-                href={`${ROOT}/dxc/dxc-components-1.0.sketch`}
-                target="_blank">Components Sticker Sheet v1.0</a>
-            </li>
-          </ul>
-
+        <h2>DXC</h2>
+          <a id="dxc-components-sketch"
+            href={`${ROOT}/dxc/dxc-components-1.0.sketch`}
+            target="_blank">Components Sticker Sheet v1.0 (Sketch)</a>
         </section>
 
         <section id="hpi">
-          <h2>HP Inc.</h2>
-
-          <h4>Adobe Illustrator</h4>
-
-          <ul>
-            <li>
-              <a id="hpi-ai-general"
-                href={`${ROOT}/hpi/hpi-sticker-sheet-apps-general-0-3.ai`}
-                target="_blank">General Sticker Sheet v0.3</a>
-            </li>
-          </ul>
-
-          <h4>Adobe PDF</h4>
-
-          <ul>
-            <li>
-             <a id="hpi-ai-general-pdf"
-                href={`${ROOT}/hpi/hpi-sticker-sheet-apps-general-0-3.pdf`}
-                target="_blank">General Sticker Sheet v0.3</a>
-            </li>
-          </ul>
-
+        <h2>HP Inc.</h2>
+          <a id="hpi-ai-general"
+            href={`${ROOT}/hpi/hpi-sticker-sheet-apps-general-0-3.ai`}
+            target="_blank">General Sticker Sheet v0.3 (Adobe Illustrator)</a>
         </section>
       </DocsArticle>
     );
